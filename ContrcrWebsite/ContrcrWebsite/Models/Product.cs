e804071cf2a31e96f65eb5b256a4b804ae1f9f83
Paata Home <paata.tepnadze@gmail.com>
2022-11-10 21:22:13 +0100

    create Products jseon /model and service

---
+++ ContrcrWebsite/ContrcrWebsite/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContrcrWebsite.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string maker { get; set; }
+
+
+        [JsonPropertyName("img")]
+        public string img { get; set; }
+        public string title { get; set; }
+        public string url { get; set; }
+        public string description { get; set; }
+        public int[] Rating { get; set; }
+
+        public override string ToString()=> JsonSerializer.Serialize(this);
+     
+    }
+}
